9602fa289b91f74cd8f53f7a8b6fcccdfaa75950
RoopsaSen <65804488+RoopsaSen@users.noreply.github.com>
2021-12-18 20:25:48 +0530

    Update model.py

--- trajectron/snce/model.py
+++ trajectron/snce/model.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
+import pytorch_lightning as pl
 
+"""
 class ProjHead(nn.Module):
     '''
     Nonlinear projection head that maps the extracted motion features to the embedding space
@@ -15,7 +17,21 @@ class ProjHead(nn.Module):
 
     def forward(self, feat):
         return self.head(feat)
+"""
 
+class ProjHead(pl.LightningModule):
+    def __init__(self, feat_dim, hidden_dim, head_dim):
+        super(ProjHead, self).__init__()
+        self.head = nn.Sequential(
+            nn.Linear(feat_dim, hidden_dim),
+            nn.ReLU(inplace=True),
+            nn.Linear(hidden_dim, head_dim)
+            )
+
+    def forward(self, feat):
+        return self.head(feat)
+        
+"""        
 class EventEncoder(nn.Module):
     '''
     Event encoder that maps an sampled event (location & time) to the embedding space
@@ -40,8 +56,51 @@ class EventEncoder(nn.Module):
         emb_state = self.spatial(state)
         emb_time = self.temporal(time)
         return self.encoder(torch.cat([emb_time, emb_state], axis=-1))
+"""
 
+class EventEncoder(pl.LightningModule):
+    '''
+    Event encoder that maps an sampled event (location & time) to the embedding space
+    '''
+    def __init__(self, hidden_dim, head_dim):
+        super(EventEncoder, self).__init__()
+        self.temporal = nn.Sequential(
+            nn.Linear(1, hidden_dim),
+            nn.ReLU(inplace=True)
+            )
+        self.spatial = nn.Sequential(
+            nn.Linear(2, hidden_dim),
+            nn.ReLU(inplace=True)
+            )
+        self.encoder = nn.Sequential(
+            nn.Linear(hidden_dim*2, hidden_dim),
+            nn.ReLU(inplace=True),
+            nn.Linear(hidden_dim, head_dim)
+        )
+
+    def forward(self, state, time):
+        emb_state = self.spatial(state)
+        emb_time = self.temporal(time)
+        return self.encoder(torch.cat([emb_time, emb_state], axis=-1))
+
+"""        
 class SpatialEncoder(nn.Module):
+    '''
+    Spatial encoder that maps an sampled location to the embedding space
+    '''
+    def __init__(self, hidden_dim, head_dim):
+        super(SpatialEncoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(2, hidden_dim),
+            nn.ReLU(inplace=True),
+            nn.Linear(hidden_dim, head_dim)
+        )
+
+    def forward(self, state):
+        return self.encoder(state)
+"""
+
+class SpatialEncoder(pl.LightningModule):
     '''
     Spatial encoder that maps an sampled location to the embedding space
     '''
